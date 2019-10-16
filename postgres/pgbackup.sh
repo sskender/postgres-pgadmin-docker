ab566d8703838d14f2da643b2234560e2dbf5ce7
sskender <sskender@users.noreply.github.com>
2019-10-16 21:03:30 +0200

    pgbackup.sh script created

--- postgres/pgbackup.sh
+++ postgres/pgbackup.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+set -e
+
+# backup all databases
+for DATABASE in `psql -At -U postgres -c "SELECT datname FROM pg_database WHERE NOT datistemplate ORDER BY datname;" postgres`
+do
+    echo "Dumping $DATABASE ..."
+    pg_dump -U postgres -Fc "$DATABASE" > /opt/backups/"$DATABASE".$(date -d "today" +"%Y-%m-%d-%H-%M").dump
+done
+
+# delete backups older than 7 days
+find /opt/backups -mtime +7 -type f -delete
